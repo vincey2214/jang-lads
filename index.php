931ea0f2d2fa8d09ffb431c21b77a299045da194
vincey2214 <66318286+vincey2214@users.noreply.github.com>
2021-08-23 01:39:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -210,14 +210,14 @@ Try refreshing the page or get in touch through our <a href="https://wetransfer.
 <br>
 <ul class="filelist">
   <li class="filelist__item"><div class="poptip"><div class="filelist__meta">
-  <span class="filelist__name" title="list of items.pdf"><b>Purchase Order.pdf</b></span>
-  <p class="filelist__size"><span>0.9 MB</span><span>pdf</span></p><div class="filelist__action"><svg viewBox="0 0 24 24"><path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12s4.477 10 10 10 10-4.477 10-10zm-9 1.492V7.998C13 7.446 12.552 7 12 7c-.556 0-1 .447-1 .998v5.48l-2.53-2.53c-.385-.385-1.022-.39-1.413.002-.393.393-.39 1.022-.002 1.412l4.247 4.247c.192.19.447.288.702.288.26.003.514-.095.708-.29l4.247-4.246c.383-.385.387-1.022-.003-1.412-.394-.393-1.023-.392-1.413-.002L13 13.492zM0 12C0 5.373 5.373 0 12 0s12 5.373 12 12-5.373 12-12 12S0 18.627 0 12z" fill-rule="evenodd"></path></svg></div></div></div></li>
+  <span class="filelist__name" title="Purchase Order.pdf"><b>Purchase Order.pdf</b></span>
+  <p class="filelist__size"><span>15 MB</span><span>zip</span></p><div class="filelist__action"><svg viewBox="0 0 24 24"><path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12s4.477 10 10 10 10-4.477 10-10zm-9 1.492V7.998C13 7.446 12.552 7 12 7c-.556 0-1 .447-1 .998v5.48l-2.53-2.53c-.385-.385-1.022-.39-1.413.002-.393.393-.39 1.022-.002 1.412l4.247 4.247c.192.19.447.288.702.288.26.003.514-.095.708-.29l4.247-4.246c.383-.385.387-1.022-.003-1.412-.394-.393-1.023-.392-1.413-.002L13 13.492zM0 12C0 5.373 5.373 0 12 0s12 5.373 12 12-5.373 12-12 12S0 18.627 0 12z" fill-rule="evenodd"></path></svg></div></div></div></li>
 </ul>
 <div style="height:8px"></div>
 <ul class="filelist">
   <li class="filelist__item"><div class="poptip"><div class="filelist__meta">
-  <span class="filelist__name" title="Specifications.pdf"><b>Specifications.pdf</b></span>
-  <p class="filelist__size"><span>5.3 MB</span><span>zip</span></p><div class="filelist__action"><svg viewBox="0 0 24 24"><path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12s4.477 10 10 10 10-4.477 10-10zm-9 1.492V7.998C13 7.446 12.552 7 12 7c-.556 0-1 .447-1 .998v5.48l-2.53-2.53c-.385-.385-1.022-.39-1.413.002-.393.393-.39 1.022-.002 1.412l4.247 4.247c.192.19.447.288.702.288.26.003.514-.095.708-.29l4.247-4.246c.383-.385.387-1.022-.003-1.412-.394-.393-1.023-.392-1.413-.002L13 13.492zM0 12C0 5.373 5.373 0 12 0s12 5.373 12 12-5.373 12-12 12S0 18.627 0 12z" fill-rule="evenodd"></path></svg></div></div></div></li>
+  <span class="filelist__name" title="Company Profile.pdf"><b>Specifications.pdf</b></span>
+  <p class="filelist__size"><span>130 MB</span><span>m4v</span></p><div class="filelist__action"><svg viewBox="0 0 24 24"><path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12s4.477 10 10 10 10-4.477 10-10zm-9 1.492V7.998C13 7.446 12.552 7 12 7c-.556 0-1 .447-1 .998v5.48l-2.53-2.53c-.385-.385-1.022-.39-1.413.002-.393.393-.39 1.022-.002 1.412l4.247 4.247c.192.19.447.288.702.288.26.003.514-.095.708-.29l4.247-4.246c.383-.385.387-1.022-.003-1.412-.394-.393-1.023-.392-1.413-.002L13 13.492zM0 12C0 5.373 5.373 0 12 0s12 5.373 12 12-5.373 12-12 12S0 18.627 0 12z" fill-rule="evenodd"></path></svg></div></div></div></li>
 </ul>
   
 
